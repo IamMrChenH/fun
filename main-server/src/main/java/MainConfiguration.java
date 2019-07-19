7424ca162bcf78dcd90233959efcf7ac4335e742
chenh <chenh@kpy.com>
2019-07-19 14:54:36 +0800

    main 配置文件

---
+++ main-server/src/main/java/MainConfiguration.java
@@ -0,0 +1,15 @@
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * @author chenh
+ * @date 2019-07-19
+ */
+@Configuration
+@ComponentScan
+@EnableJpaRepositories
+@EntityScan
+public class MainConfiguration {
+}
--- main-server/src/main/java/com/fun/main/server/MainServerApplication.java
+++ main-server/src/main/java/com/fun/main/server/MainServerApplication.java
@@ -2,10 +2,12 @@ package com.fun.main.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@ComponentScan(basePackages = "com.fun")
 public class MainServerApplication {
 
     public static void main(String[] args) {
