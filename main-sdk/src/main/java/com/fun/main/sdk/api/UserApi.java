d454560f5d69f1ca529ec5bc44ed5bfed7587416
chenh <chenh@kpy.com>
2019-07-19 12:07:00 +0800

    父类包 pom.xml 打包改pom 修复编译问题

--- main-sdk/src/main/java/com/fun/main/sdk/api/UserApi.java
+++ main-sdk/src/main/java/com/fun/main/sdk/api/UserApi.java
@@ -12,21 +12,20 @@ import java.util.List;
  * @author IamMr
  * @date 2019年07月16日 —— 23:11
  */
-@Api("userApi")
-@ApiModel("用户接口")
+@Api(value = "user",tags = "用户管理接口")
 @RequestMapping("/v1/user")
 public interface UserApi {
 
-    @ApiOperation(value = "获取用户信息", notes = "根据url的id获取用户信息")
+    @ApiOperation(value = "根据id获取用户信息", notes = "根据url的id获取用户信息")
     @GetMapping("{id}")
     UserVO getUserById(@PathVariable("id") @ApiParam(name = "id", value = "用户ID", required = true) Long id);
 
     @ApiOperation(value = "获取用户列表", notes = "获取用户的列表")
-    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
+    @ApiImplicitParam
     @GetMapping
     List<UserVO> getUserListPage();
 
-    @ApiOperation(value = "获取用户信息", notes = "根据url的id获取用户信息")
+    @ApiOperation(value = "根据名称获取用户信息", notes = "根据名称获取用户信息")
     @GetMapping("username/{name}")
     UserVO getUserByName(@PathVariable("name") @ApiParam(name = "name", value = "用户登录名", required = true) String name);
 }
