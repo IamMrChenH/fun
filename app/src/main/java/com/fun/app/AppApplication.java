ac2519397bab26c5a1a85bae1d5641926550dbc8
chenh <chenh@kpy.com>
2019-07-22 14:34:02 +0800

    调整

--- app/src/main/java/com/fun/app/AppApplication.java
+++ app/src/main/java/com/fun/app/AppApplication.java
@@ -3,14 +3,13 @@ package com.fun.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.fun")
 public class AppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 
 }
